e59e88d69672156670e381d035c916234fb6c3a0
Dikasteria <rjhey@hotmail.com>
2021-05-01 15:39:19 +0100

    amendments to events and queries

--- events/index.js
+++ events/index.js
@@ -8,8 +8,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
+
+    events.push(event);
+
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:4001/events', event);
     axios.post('http://localhost:4002/events', event);
@@ -17,6 +22,11 @@ app.post('/events', (req, res) => {
 
     res.send({status: 'OK'});
 });
+
+
+app.get('/events', (req, res) => {
+    res.send(events);
+})
 
 app.listen(4005, () => {
     console.log("listening on port 4005")
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const {type, data} = req.body;
-    
+const handleEvent = (data, type) => {
     if (type === "PostCreated") {
         const {id, title} = data;
         posts[id] = {id, title, comments: []};
@@ -35,11 +30,26 @@ app.post('/events', (req, res) => {
         comment.content = content;
         console.log(comment)
     };
-    
+}
 
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const {type, data} = req.body;
+    
+    handleEvent(data, type);
+    
     res.send({});
 });
 
-app.listen(4002, () => {
-    console.log("Listening on port 4002")
+app.listen(4002, async() => {
+    console.log("Listening on port 4002");
+
+    const res = await axios.get('http://localhost:4005/events');
+    for (let event of res.data) {
+        console.log("Processing event", event.type);
+        handleEvent(event.data, event.type);
+    }
 });
